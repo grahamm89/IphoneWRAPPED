6543e829a3464d7805b61d20c8bb2136405dea33
grahamm89 <116651536+grahamm89@users.noreply.github.com>
2025-07-27 14:52:11 +0100

    Delete generate-keys.js

--- generate-keys.js
+++
@@ -1,4 +0,0 @@
-import webPush from 'web-push';
-const { publicKey, privateKey } = webPush.generateVAPIDKeys();
-console.log('VAPID_PUBLIC_KEY=', publicKey);
-console.log('VAPID_PRIVATE_KEY=', privateKey);
